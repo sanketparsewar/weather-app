4c690ac572acc041f53018d9bfe4107fc870ab76
sanket parsewar <97521747+sanketparsewar@users.noreply.github.com>
2024-09-24 21:36:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const apiKey = "7fde13c63bc123f8f29d55510adc7d98";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+  //   console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "clouds.png";
+  }
+  document.querySelector(".weather").style.display = "block";
+}
+
+searchBtn.addEventListener("click", function () {
+  checkWeather(searchBox.value);
+});
